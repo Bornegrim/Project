6afc905df1a5c19196861804de54ad618fac6a7c
Gustav Holmquist <gustavhlmqst@gmail.com>
2018-05-08 14:42:30 +0200

    La till knappar i index och phpfiler de länkar till

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"></meta>
+  <link rel="stylesheet" type="text/css" href="Assets/css/main.css">
+  <script src="Assets/js/main.js"></script>
+</head>
+<body>
+  <div class="header">
+    <h1>Studentgatans Bostadsförening: TVÄTTSTUGAN</h1>
+  </div>
+  <div class="buttons">
+    <form action="booking.php">
+      <input class="booking_button" type="submit" name="booking" value="Boka Tid">
+    </form>
+    <form action="complaint.php">
+      <input class="complaint_button" type="submit" name="complaint" value="Gnäll">
+    </form>
+    <form action="createaccount.php">
+      <input class="create_acc_button" type="submit" name="create_account" value="Skapa Konto">
+    </form>
+  </div>
+</body>
+</html>
