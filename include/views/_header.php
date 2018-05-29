14b3083f1f102586187af3eac4669663838c9cc8
Kraelich <kraelich@gmail.com>
2018-05-29 20:15:02 +0200

    Header-loggan tar en nu till index.php

--- include/views/_header.php
+++ include/views/_header.php
@@ -2,7 +2,7 @@
 <html lang="swe" dir="ltr">
 <header class="header">
   <ul class="headerlist">
-    <li class="headerlist_img"><img src="Assets/img/Tvattbjornen_AB_rund.jpg" alt="Tvättbjörnen AB" class="header_img"></li>
+    <li class="headerlist_img"><a href="index.php"><img src="Assets/img/Tvattbjornen_AB_rund.jpg" alt="Tvättbjörnen AB" class="header_img"></li>
     <li class="headerlist_item"><a href="logout-process.php">Logga ut</a></li>
     <li class="headerlist_item"><a href="terms_conditions.php">Användarvillkor</a></li>
     <li class="headerlist_item"><p><?php echo "Välkommen " . $_SESSION['Name']; ?></p></li>
