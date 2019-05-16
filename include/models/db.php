31b65327de883c36e394ee7ba0f52a8d3b68b1d1
Lucas Bornegrim <38943717+Bornegrim@users.noreply.github.com>
2019-05-16 22:44:32 +0200

    Update db.php

--- include/models/db.php
+++ include/models/db.php
@@ -7,10 +7,10 @@ class Db {
   private $dbname;
 
   protected function connect() {
-    $this->host = "n3plcpnl0118.prod.ams3.secureserver.net";
-    $this->uname = "Bornelu";
-    $this->pass = "Gitgud@@@123";
-    $this->dbname = "Bornegrim";
+    $this->host = "";
+    $this->uname = "";
+    $this->pass = "";
+    $this->dbname = "";
 
     $conn = new mysqli($this->host, $this->uname, $this->pass, $this->dbname);
     return $conn;
